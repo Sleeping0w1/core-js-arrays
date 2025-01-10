4d33dc0dcdd075f667831a51d90b3fee9c31fb29
Sleeping0w1 <1033351@gmail.com>
2025-01-10 23:58:33 +0300

    feat: solution for getIndicesOfOddNumbers

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -432,8 +432,9 @@ function getIdentityMatrix(n) {
  *    getIndicesOfOddNumbers([2, 4, 6, 8, 10]) => []
  *    getIndicesOfOddNumbers([11, 22, 33, 44, 55]) => [0, 2, 4]
  */
-function getIndicesOfOddNumbers(/* numbers */) {
-  throw new Error('Not implemented');
+function getIndicesOfOddNumbers(numbers) {
+  const odd = numbers.filter((el) => el % 2 > 0);
+  return odd.map((el) => numbers.indexOf(el));
 }
 
 /**
